70cf9bd91fba91e20bae07c48aa708c81bce1422
kocairiserkan <serkankocairi@outlook.com>
2024-03-14 00:41:58 +0000

    order some pizzas

---
+++ src/features/cart/CartOverview.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import {formatCurrency} from '../../utils/helpers';
+
+function CartOverview() {
+
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if(!totalCartQuantity) return null;
+
+  return (
+    <div className=" flex items-center justify-between uppercase px-4 py-4 text-sm bg-stone-800 text-stone-200 sm:px-6 md:text-base">
+      <p className="text-stone-300 space-x-4 font-semibold sm:space-x-6">
+        <span>{totalCartQuantity} {totalCartQuantity > 1 ? 'pizzas' : 'pizza'}</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
+      </p>
+      <Link to = '/cart'>Open cart &rarr;</Link>
+    </div>
+  );
+}
+
+export default CartOverview;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import UserName from "../features/user/UserName";
+
+function Header() {
+    return (
+        <header className=" flex items-center justify-between border-b border-stone-200 bg-yellow-400 uppercase px-4 py-3 sm:px-6 ">
+            <Link to="/" className="tracking-[5px]">Fast React Pizza Co.</Link>
+            <SearchOrder/>
+            <UserName/>
+        </header>  
+    );
+}
+
+export default Header;
